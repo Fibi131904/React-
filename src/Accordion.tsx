208d71ff07785c971be0277c301f81aa38eba3a0
Kseniya Smirnova <fanni@bk.ru>
2022-02-06 10:08:54 +0300

    PropsType

---
+++ src/Accordion.tsx
@@ -0,0 +1,47 @@
+import { type } from 'os';
+import React from 'react';
+import { isReturnStatement } from 'typescript';
+
+type AccordionPropsType = {
+  titleValue: string
+  collapsed: boolean
+}
+
+export function Accordion(props: AccordionPropsType) {
+
+  if (props.collapsed) {
+    return <div>
+      <AccordionTitle title={props.titleValue} />
+    </div>
+  } else {
+    return <div>
+      <AccordionTitle title={props.titleValue} />
+      <AccordionBody />
+    </div>
+
+  }
+}
+
+
+type AccordionTitlePropsType = {
+  title: string
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+
+  return (
+    <h3>{props.title}</h3>);
+}
+
+
+function AccordionBody() {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+
+  );
+}
+
