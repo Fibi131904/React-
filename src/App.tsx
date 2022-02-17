36c9a67110620040abdce2909c5de9454be95809
Kseniya Smirnova <fanni@bk.ru>
2022-02-17 20:51:07 +0300

    06,07

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 import { Accordion } from './Accordion';
 import { Rating } from './Rating';
-
+import OnOff from './OnOff';
+import UncontrolledAccordion  from './UncontrolledAccordion';
+import { AncontrolledRating } from './UncontrolledRating';
 
 
 function App() {
   return (
     <div>
-      <PageTitle title={'This is App component'} />
+
+      <OnOff />
+      <UncontrolledAccordion titleValue={'Menu'}/>
+      <AncontrolledRating />
+
+      <Rating value={3} />
+      <Accordion titleValue={'Menu'} collapsed={false}/>
+      {/* <PageTitle title={'This is App component'} />
       <PageTitle title={'Frends'} />
       Article 1
       <Rating value={3}/>
-      <Accordion titleValue={'Menu'} collapsed= {true}/>
-      <Accordion titleValue={'Users'} collapsed= {false}/>
+      
       Article 2 
       <Rating value={0}/>
       <Rating value={1}/>
-      <Rating value={2}/>
+      
       <Rating value={3}/>
       <Rating value={4}/>
-      <Rating value={5}/>
+      <Rating value={5}/> */}
           </div>
   );
 }
